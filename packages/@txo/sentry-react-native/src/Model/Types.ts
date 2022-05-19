801e84d83887ea5a66b75a2f12ff59e4ecc1018b
Erik Slovak <erik.slovak@technologystudio.sk>
2022-05-19 11:52:07 +0200

    use sentry type for options and add debug flag

--- packages/@txo/sentry-react-native/src/Model/Types.ts
+++ packages/@txo/sentry-react-native/src/Model/Types.ts
@@ -6,7 +6,7 @@
 
 import type { NavigationState as _NavigationState } from 'react-navigation'
 import type { DefaultRootState } from 'react-redux'
-import type { Options } from '@sentry/types'
+import type { ReactNativeOptions } from '@sentry/react-native'
 
 export type NavigationState = _NavigationState & {
   routeName?: string,
@@ -16,11 +16,8 @@ export type RootState = {
   navigation: NavigationState,
 }
 
-export type SentryConfig = {
+export type SentryConfig = Partial<ReactNativeOptions> & {
   baseUrl: string,
-  dsn: string,
-  environment: string,
-  tracesSampleRate?: number,
   userIdSelector: (state: DefaultRootState) => string | null,
-  beforeBreadcrumb?: Options['beforeBreadcrumb'],
+  debug?: boolean,
 }
